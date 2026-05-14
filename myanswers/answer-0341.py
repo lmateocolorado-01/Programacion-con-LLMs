12fb0ff8eb7c0b97314658a9d9d8853a4c773080
lmateocolorado-01 <lmateo.colorado@udea.edu.co>
2026-05-14 16:58:06 +0200

    Update answer-0341.py

--- myanswers/answer-0341.py
+++ myanswers/answer-0341.py
@@ -1,42 +1,7 @@
-import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-
+import numpy as np
 
 def preparar_datos(dataset, features, target):
     X = dataset[features]
     y = dataset[target]
     return X, y
-
-
-def predecir_cosecha_gulupa(dataset, condiciones_actuales):
-    features = [
-        "temp_promedio",
-        "humedad_rel",
-        "precipitacion_mm",
-        "edad_cultivo_sem"
-    ]
-
-    target = "produccion_kg"
-
-    X, y = preparar_datos(dataset, features, target)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42
-    )
-
-    modelo = LinearRegression()
-    modelo.fit(X_train, y_train)
-
-    condiciones_actuales = pd.DataFrame(
-        [condiciones_actuales],
-        columns=features
-    )
-
-    prediccion = modelo.predict(condiciones_actuales)
-
-    return float(prediccion[0])
